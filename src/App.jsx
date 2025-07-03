52017e5124dee9fab9fae0e657104d259178bde1
Dama-27 <damathdesilva@gmail.com>
2025-07-03 10:07:05 +0530

    tailwind-absolue,fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,26 @@ import ProductCard from './components/productaCard'
 function App() {
 
   return (
-    <>
-      <Header/>
+    <div className='w-full h-screen bg-red-100 flex flex-col justify-center items-center'>
+      <div className='relative w-[650px] h-[650px] bg-red-900 flex flex-col justify-center items-center'>
+        <div className='w-[600px] h-[600px] bg-green-500 flex flex-col justify-center items-center'>
+          <div className='w-[100px] h-[100px] bg-blue-600 absolute bottom-[0px] right-[0px]'></div>
+          {/* by using fixed we can move arbitary, we can use absolute also but fixed is powefull */}
+          <div className='w-[100px] h-[100px] bg-green-600'></div>
+          <div className='w-[100px] h-[100px] bg-yellow-600'></div>
+          <div className='w-[100px] h-[100px] bg-purple-600'></div>
+          <div className='w-[100px] h-[100px] bg-pink-600'></div>
+          <div className='w-[100px] h-[100px] bg-orange-600'></div>
+        </div>
+      </div>
+      
+      
+      {/* <Header/>
       <ProductCard name="dada dada dadad dada" description="lorem lorem lorem" price="$1000/=" picture={"https://picsum.photos/id/1/200/300"}/>
       <ProductCard name="Apple laptop" description="wadak na" price="1000/=" picture={"https://picsum.photos/id/2/200/300"}/>
       <ProductCard name="MSI laptop" description="wadak na" price="1000/=" picture={"https://picsum.photos/id/3/200/300"}/>
-      <ProductCard/>
-    </>
+      <ProductCard/> */}
+    </div>
   )
 }
 
