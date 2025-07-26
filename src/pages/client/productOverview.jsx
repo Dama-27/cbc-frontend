2bccb9a302e7a708c11c2dcc2842b3aac912d482
Dama-27 <damathdesilva@gmail.com>
2025-07-26 13:36:46 +0530

    product details in overview page complete

--- src/pages/client/productOverview.jsx
+++ src/pages/client/productOverview.jsx
@@ -40,7 +40,36 @@ export default function ProductoverviewPage(){
                 <div className="w-[50%] h-full flex justify-center items-center">
                     <ImageSlider images = {product.images}/>
                 </div>
-                <div className="w-[50%] bg-blue-900 h-full">
+                <div className="w-[50%] h-full flex justify-center items-center">
+                    <div className="w-[500px] h-[500px] flex flex-col items-center">
+                        <h1 className="w-full text-center text-4xl text-secondary font-semibold">{product.name}
+                            {
+                                product.altNames.map((altName,index)=>{
+                                    return(
+                                        <span key={index} className="text-4xl text-gray-600"> | {altName}</span>
+                                    )
+                                })
+                            }
+                        </h1>
+                        {/* product id */}
+                        <h2 className="w-full text-center my-2 text-gray-600 mt-2">{product.productId}</h2>
+                        <p className="w-full text-center my-2 text-gray-600 mt-2">{product.description}</p>
+                        {
+                            product.labelledPrice > product.price ?
+                            <div>
+                                <span className="text-4xl mx-4 text-gray-500 line-through">{product.labelledPrice.toFixed(2)}</span>
+                                <span className="text-4xl mx-4 font-bold text-accent">{product.price.toFixed(2)}</span>
+                            </div>
+                            : <span className="text-4xl mx-4 font-bold text-accent">{product.price.toFixed(2)}</span>
+                        }
+                        <div className="w-full flex justify-center">
+                            <button className="w-1/2 h-12 mx-4 cursor-pointer bg-accent text-white font-semibold rounded-md hover:bg-secondary">Buy Now</button>
+                            <button className="w-1/2 h-12 mx-4 cursor-pointer bg-accent text-white font-semibold rounded-md hover:bg-secondary">Add to Cart</button>
+                        </div>
+
+                        
+                    </div>
+                    
 
                 </div>
             </div>
