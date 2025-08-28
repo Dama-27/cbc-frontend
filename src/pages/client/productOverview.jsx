15b243168b922249b37a1a3a352e3c607c2991d5
Dama-27 <damathdesilva@gmail.com>
2025-08-28 07:18:05 +0530

    sending cart to checkout updated

--- src/pages/client/productOverview.jsx
+++ src/pages/client/productOverview.jsx
@@ -1,98 +1,116 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom";
 import ImageSlider from "../../components/imageSlider";
 import Loading from "../../components/loading";
 import { addToCart, getCart } from "../../utils/cart";
 
-export default function ProductoverviewPage(){
-    const params = useParams();
-    const productId = params.id;
-    const [status, setStatus] = useState("loading") // sucess, error
-    const [product, setProduct] = useState(null)
+export default function ProductoverviewPage() {
+  const params = useParams();
+  const productId = params.id;
+  const [status, setStatus] = useState("loading"); // sucess, error
+  const [product, setProduct] = useState(null);
+  const navigate = useNavigate();
 
-
-    useEffect(
-        ()=>{
-            axios.get(import.meta.env.VITE_BACKEND_URL + "/api/products/" + productId).then(
-                (res)=>{
-                    console.log(res.data)
-                    setProduct(res.data)
-                    setStatus("success")
-                }
-            ).catch(
-                (error)=>{
-                    console.log(error)
-                    setStatus("error")
-                    toast.error("Error fetching product details")
-
-                }
-            )
-        },[]
-    )
-    return(
-        <>
-        {
-            status == "success" && 
-            (
-            <div className=" w-full h-full flex">
-                {/* This is OverView {JSON.stringify(product)} */}
-                <div className="w-[50%] h-full flex justify-center items-center">
-                    <ImageSlider images = {product.images}/>
+  useEffect(() => {
+    axios
+      .get(import.meta.env.VITE_BACKEND_URL + "/api/products/" + productId)
+      .then((res) => {
+        console.log(res.data);
+        setProduct(res.data);
+        setStatus("success");
+      })
+      .catch((error) => {
+        console.log(error);
+        setStatus("error");
+        toast.error("Error fetching product details");
+      });
+  }, []);
+  return (
+    <>
+      {status == "success" && (
+        <div className=" w-full h-full flex">
+          {/* This is OverView {JSON.stringify(product)} */}
+          <div className="w-[50%] h-full flex justify-center items-center">
+            <ImageSlider images={product.images} />
+          </div>
+          <div className="w-[50%] h-full flex justify-center items-center">
+            <div className="w-[500px] h-[500px] flex flex-col items-center">
+              <h1 className="w-full text-center text-4xl text-secondary font-semibold">
+                {product.name}
+                {product.altNames.map((altName, index) => {
+                  return (
+                    <span key={index} className="text-4xl text-gray-600">
+                      {" "}
+                      | {altName}
+                    </span>
+                  );
+                })}
+              </h1>
+              {/* product id */}
+              <h2 className="w-full text-center my-2 text-gray-600 mt-2">
+                {product.productId}
+              </h2>
+              <p className="w-full text-center my-2 text-gray-600 mt-2">
+                {product.description}
+              </p>
+              {product.labelledPrice > product.price ? (
+                <div>
+                  <span className="text-4xl mx-4 text-gray-500 line-through">
+                    {product.labelledPrice.toFixed(2)}
+                  </span>
+                  <span className="text-4xl mx-4 font-bold text-accent">
+                    {product.price.toFixed(2)}
+                  </span>
                 </div>
-                <div className="w-[50%] h-full flex justify-center items-center">
-                    <div className="w-[500px] h-[500px] flex flex-col items-center">
-                        <h1 className="w-full text-center text-4xl text-secondary font-semibold">{product.name}
-                            {
-                                product.altNames.map((altName,index)=>{
-                                    return(
-                                        <span key={index} className="text-4xl text-gray-600"> | {altName}</span>
-                                    )
-                                })
-                            }
-                        </h1>
-                        {/* product id */}
-                        <h2 className="w-full text-center my-2 text-gray-600 mt-2">{product.productId}</h2>
-                        <p className="w-full text-center my-2 text-gray-600 mt-2">{product.description}</p>
-                        {
-                            product.labelledPrice > product.price ?
-                            <div>
-                                <span className="text-4xl mx-4 text-gray-500 line-through">{product.labelledPrice.toFixed(2)}</span>
-                                <span className="text-4xl mx-4 font-bold text-accent">{product.price.toFixed(2)}</span>
-                            </div>
-                            : <span className="text-4xl mx-4 font-bold text-accent">{product.price.toFixed(2)}</span>
-                        }
-                        <div className="w-full flex justify-center">
-                            <button className="w-1/2 h-12 mx-4 cursor-pointer bg-accent text-white font-semibold rounded-md hover:bg-secondary" onClick={
-                                ()=>{
-                                    console.log("Old cart")
-                                    console.log(getCart())
-                                    
-                                    addToCart(product,1)
-                                    console.log("New cart") 
-                                    console.log(getCart())
-                                }
-                                
-                            }>Add to Cart</button>
-                            <button className="w-1/2 h-12 mx-4 cursor-pointer bg-accent text-white font-semibold rounded-md hover:bg-secondary">Buy Now</button>
-                        </div>
+              ) : (
+                <span className="text-4xl mx-4 font-bold text-accent">
+                  {product.price.toFixed(2)}
+                </span>
+              )}
+              <div className="w-full flex justify-center">
+                <button
+                  className="w-1/2 h-12 mx-4 cursor-pointer bg-accent text-white font-semibold rounded-md hover:bg-secondary"
+                  onClick={() => {
+                    console.log("Old cart");
+                    console.log(getCart());
 
-                        
-                    </div>
-                    
-
-                </div>
+                    addToCart(product, 1);
+                    console.log("New cart");
+                    console.log(getCart());
+                  }}
+                >
+                  Add to Cart
+                </button>
+                <button
+                  className="w-1/2 h-12 mx-4 cursor-pointer bg-accent text-white font-semibold rounded-md hover:bg-secondary"
+                  onClick={() => {
+                    navigate("/checkout", {
+                      state: {
+                        cart: [
+                          {
+                            productId: product.productId,
+                            name: product.name,
+                            price: product.price,
+                            labelledPrice: product.labelledPrice,
+                            image: product.images[0],
+                            qty: 1,
+                          },
+                        ],
+                      },
+                    });
+                  }}
+                >
+                  Buy Now
+                </button>
+              </div>
             </div>
-            )
-        }
+          </div>
+        </div>
+      )}
 
-        {
-            
-            status == "loading" && <Loading />
-            
-            
-        }
-        </>
-    )
+      {status == "loading" && <Loading />}
+    </>
+  );
 }
